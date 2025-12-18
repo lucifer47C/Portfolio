08b1eb0bfab48831fc1f8c4e390224da5b2a88ce
Steve Yadav <steveyadav.2002newdelhi@gmail.com>
2025-12-18 14:52:54 +0530

    Internship Archive v0.2

--- src/components/sections/internships-section.tsx
+++ src/components/sections/internships-section.tsx
@@ -43,7 +43,7 @@ export default function InternshipsSection() {
       </div>
       <div className="mt-12 text-center">
         <Button asChild variant="link" className="group text-lg">
-            <Link href="/archive/internshippage.tsx">
+            <Link href="/archive/internships">
                 View Full Internship Archive
                 <ArrowRight className="ml-2 h-5 w-5 transition-transform group-hover:translate-x-1" />
             </Link>
